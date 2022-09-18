2150dbccf177e7323b8ba26c5448d482afd6d8c1
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2022-09-18 00:43:54 -0400

    Time: 138 ms (41.53%), Space: 15.1 MB (64.24%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in dict:
+                return [dict[diff], i]
+            dict[num] = i
+            
+            
