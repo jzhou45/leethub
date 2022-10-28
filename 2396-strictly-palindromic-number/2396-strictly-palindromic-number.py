ab4cf702111b01083644d85bd0fb73467d0b3440
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2022-10-27 22:21:40 -0400

    Time: 282 ms (5.00%), Space: 31.7 MB (14.62%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -2,12 +2,4 @@ import numpy as np
 
 class Solution:
     def isStrictlyPalindromic(self, n: int) -> bool:
-        for i in range(2, n - 1):
-            temp = np.base_repr(n, base=i)
-            comp = temp
-            temp = [*temp]
-            temp.reverse()
-            temp = "".join(temp)
-            if comp != temp:
-                return False
-        return True
+        return False
