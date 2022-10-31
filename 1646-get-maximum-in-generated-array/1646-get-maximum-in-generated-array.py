dcde8399ab7140b6e5561d109060d3878995561d
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2022-10-31 01:35:40 -0400

    Time: 48 ms (71.72%), Space: 14 MB (17.41%) - LeetHub

--- 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.py
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.py
@@ -2,15 +2,16 @@ class Solution:
     def getMaximumGenerated(self, n: int) -> int:
         if n == 0: return 0
         
-        nums = [float("-inf")] * (n + 1)
+        nums = [0] * (n + 1)
         nums[0] = 0
         nums[1] = 1
+        res = 1
         
         for i in range(len(nums) + 1):
             if 2 <= 2 * i <= n:
                 nums[2 * i] = nums[i]
+                res = max(nums[2* i], res)
             if 2 <= 2 * i + 1 <= n:
                 nums[2 * i + 1] = nums[i] + nums[i + 1]
-                
-        print(nums)
-        return max(nums)
+                res = max(nums[2 * i + 1], res)
+        return res
