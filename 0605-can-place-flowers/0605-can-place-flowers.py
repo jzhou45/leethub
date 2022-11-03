64e9dc8611cd559ecbab6d83ed812d3dc770036b
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2022-11-03 01:56:02 -0400

    Time: 487 ms (10.11%), Space: 14.4 MB (30.83%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -7,34 +7,32 @@ class Solution:
             return True if n < 2 else False
         
         l, m, r = 0, 1, 2
+        
+        def travel(i):
+            nonlocal l
+            nonlocal m
+            nonlocal r
+            l = i + 1
+            m = l + 1
+            r = m + 1
+            
         if flowerbed[0] == 0 and flowerbed[1] == 0:
             flowerbed[0] = 1
             n -= 1
-            l += 1
-            m = l + 1
-            r = m + 1
+            travel(0)
         while r < len(flowerbed):
             if flowerbed[l] == 0 and flowerbed[m] == 0 and flowerbed[r] == 0:
                 flowerbed[m] = 1
                 n -= 1
-                l = m + 1
-                m = l + 1
-                r = m + 1
+                travel(m)
             elif flowerbed[r] == 1:
-                l = r + 1
-                m = l + 1
-                r = m + 1
+                travel(r)
             elif flowerbed[m] == 1:
-                l = m + 1
-                m = l + 1
-                r = m + 1
+                travel(m)
             else:
-                l += 1
-                m += 1
-                r += 1
+                travel(l)
         if flowerbed[-1] == 0 and flowerbed[-2] == 0:
             flowerbed[-1] = 1
             n -= 1
-        
         
         return n <= 0
