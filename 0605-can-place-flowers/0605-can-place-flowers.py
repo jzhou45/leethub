ae6cda2bf75412c43270ed16a6144f164d98ef62
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2022-11-03 01:53:51 -0400

    Time: 180 ms (88.24%), Space: 14.5 MB (30.83%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -6,14 +6,35 @@ class Solution:
                     return False if n > 0 else True
             return True if n < 2 else False
         
-        for i in range(len(flowerbed)):
-            if i == 0 and flowerbed[i] == 0 and flowerbed[i + 1] == 0:
-                flowerbed[i] = 1
+        l, m, r = 0, 1, 2
+        if flowerbed[0] == 0 and flowerbed[1] == 0:
+            flowerbed[0] = 1
+            n -= 1
+            l += 1
+            m = l + 1
+            r = m + 1
+        while r < len(flowerbed):
+            if flowerbed[l] == 0 and flowerbed[m] == 0 and flowerbed[r] == 0:
+                flowerbed[m] = 1
                 n -= 1
-            elif i == len(flowerbed) - 1 and flowerbed[i] == 0 and flowerbed[i - 1] == 0:
-                flowerbed[i] = 1
-                n -= 1
-            elif flowerbed[i - 1] == 0 and flowerbed[i] == 0 and flowerbed[i + 1] == 0:
-                flowerbed[i] = 1
-                n -= 1
+                l = m + 1
+                m = l + 1
+                r = m + 1
+            elif flowerbed[r] == 1:
+                l = r + 1
+                m = l + 1
+                r = m + 1
+            elif flowerbed[m] == 1:
+                l = m + 1
+                m = l + 1
+                r = m + 1
+            else:
+                l += 1
+                m += 1
+                r += 1
+        if flowerbed[-1] == 0 and flowerbed[-2] == 0:
+            flowerbed[-1] = 1
+            n -= 1
+        
+        
         return n <= 0
