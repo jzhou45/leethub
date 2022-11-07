1309f8e75e94c8b52ecaae717c4df1b507ee981f
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2022-11-06 20:28:07 -0500

    Time: 65 ms (15.34%), Space: 13.8 MB (54.69%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        num = str(num)
+        for i, char in enumerate(num):
+            if char == "6":
+                return num[:i] + "9" + num[i + 1:]
+                
+        return int(num)
