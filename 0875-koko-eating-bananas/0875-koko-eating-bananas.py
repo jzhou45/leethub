b7cf6ce564c5364885e541dc61162e28f86bb025
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2022-11-18 11:17:05 -0500

    Time: 2066 ms (5.01%), Space: 15.6 MB (24.97%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,6 +1,6 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l, r = 1, max(piles)
+        l, r = math.ceil(sum(piles)/h), max(piles)
         res = float("inf")
         
         while l <= r:
