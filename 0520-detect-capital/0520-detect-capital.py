feca63a3cd6630437d2e959eb2bd80140f1b31f4
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2023-01-02 04:24:30 -0500

    Time: 34 ms (84.76%), Space: 13.8 MB (96.54%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.py
+++ 0520-detect-capital/0520-detect-capital.py
@@ -1,3 +1,19 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-        return word == word.lower() or word == word.upper() or word == word.capitalize()
+        if word[0].isupper():
+            if len(word) == 1:
+                return True
+            
+            if word[1].isupper():
+                for i in range(2, len(word)):
+                    if word[i].islower():
+                        return False
+            else:
+                for i in range(2, len(word)):
+                    if word[i].isupper():
+                        return False
+        else:
+            for i in range(1, len(word)):
+                if word[i].isupper():
+                    return False
+        return True
