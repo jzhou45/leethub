08bd59641e9d060bdcbe5a682ad3c4f9ca8031ed
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2023-02-01 15:38:43 -0500

    Time: 100 ms (5.93%), Space: 13.9 MB (70.52%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,9 +1,13 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         res = ""
-        for i in range(len(str2)):
-            arr = str1.split(str2[:i + 1])
-            arr2 = str2.split(str1[:i + 1])
-            if len("".join(arr)) == 0 and len("".join(arr2)) == 0 and (len(str2[:i + 1]) < len(str1) or len(str2[:i + 1]) == len(str1) == len(str2)):
-                res = str2[:i + 1]
+        i = 0
+        while i < len(str1) and i < len(str2):
+            substring = str2[:i + 1]
+            arr1 = str1.split(substring)
+            arr2 = str2.split(substring)
+            if len("".join(arr1)) == 0 and len("".join(arr2)) == 0:
+                res = substring
+            i += 1
         return res
+            
