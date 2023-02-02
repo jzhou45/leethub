1b349a0f48a946332b42ff18509d81d48c3b6ebc
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2023-02-02 18:02:15 -0500

    Time: 71 ms (98.33%), Space: 43.2 MB (99.87%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function(s) {
+    let arr = []
+    for (let i = 0; i < 75; i++){
+        arr.push([0, i]);
+    };
+    
+    for (let j = 0; j < s.length; j++){
+        arr[s.charCodeAt(j) - 48][0] ++;
+    };
+    
+    arr.sort((a, b) => {
+        if (a[0] > b[0]){
+            return -1;
+        } else if (a[0] < b[0]){
+            return 1;
+        } else{
+            return 0
+        };
+    });
+    
+    let res = "";
+    
+    for (let subarr of arr){
+        let [freq, char] = subarr;
+        if (freq === 0){
+            break;
+        };
+        res += String.fromCharCode(48 + char).repeat(freq)
+    };
+    
+    return res;
+};
