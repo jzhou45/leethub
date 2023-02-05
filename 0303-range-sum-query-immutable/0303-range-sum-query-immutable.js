6a6364781ccab7d881bad757e258a0ffdd44e50e
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2023-02-05 15:13:54 -0500

    Time: 490 ms (12.50%), Space: 52.5 MB (5.00%) - LeetHub

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+    this.nums = nums;
+};
+
+/** 
+ * @param {number} left 
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(left, right) {
+    return this.nums.slice(left, right + 1).reduce((currSum, num) => currSum + num, 0);
+};
+
+/** 
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
