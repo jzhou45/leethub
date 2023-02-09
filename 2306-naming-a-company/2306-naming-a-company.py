92f30cd56049598d66abd4106da08cfba4969f52
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2023-02-09 18:59:27 -0500

    Time: 573 ms (86.02%), Space: 28.5 MB (58.06%) - LeetHub

---
+++ 2306-naming-a-company/2306-naming-a-company.py
@@ -0,0 +1,12 @@
+class Solution:
+    def distinctNames(self, ideas: List[str]) -> int:
+        A = [set() for _ in range(26)]
+        for idea in ideas:
+            A[ord(idea[0]) - ord('a')].add(idea[1:])
+        
+        ans = 0
+        for i in range(25):
+            for j in range(i + 1, 26):
+                k = len(A[i] & A[j]) # Number of duplicated suffixes
+                ans += 2 * (len(A[i]) - k) * (len(A[j]) - k)
+        return ans
