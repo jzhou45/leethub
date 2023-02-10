835bd65bf045a0baf33dd6f4bde26e95542f9abd
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2023-02-09 19:04:14 -0500

    Time: 586 ms (83.87%), Space: 28.6 MB (43.01%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.py
+++ 2306-naming-a-company/2306-naming-a-company.py
@@ -1,12 +1,18 @@
 class Solution:
     def distinctNames(self, ideas: List[str]) -> int:
-        A = [set() for _ in range(26)]
+        alphabet = [set() for _ in range(26)]
+        
         for idea in ideas:
-            A[ord(idea[0]) - ord('a')].add(idea[1:])
+            # ord(idea[0]) grabs unicode for first letter, subtracting it
+            # by ord("a") returns the index in the alphabet arr associated with
+            # the first letter
+            alphabet[ord(idea[0]) - ord('a')].add(idea[1:])
         
         ans = 0
+        
         for i in range(25):
             for j in range(i + 1, 26):
-                k = len(A[i] & A[j]) # Number of duplicated suffixes
-                ans += 2 * (len(A[i]) - k) * (len(A[j]) - k)
+                k = len(alphabet[i] & alphabet[j]) # Number of duplicated suffixes
+                ans += 2 * (len(alphabet[i]) - k) * (len(alphabet[j]) - k)
+                
         return ans
