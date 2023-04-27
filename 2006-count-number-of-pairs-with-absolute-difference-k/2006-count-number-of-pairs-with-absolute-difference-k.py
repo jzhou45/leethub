379ae6389ec5d4b20cba0969cc59403e5ce370c4
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2023-04-27 01:55:01 -0400

    Time: 260 ms (19.86%), Space: 16.3 MB (11.81%) - LeetHub

---
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countKDifference(self, nums: List[int], k: int) -> int:
+        res = 0
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if abs(nums[i] - nums[j]) == k:
+                    res += 1
+        return res
