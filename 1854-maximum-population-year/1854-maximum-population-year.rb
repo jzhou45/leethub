eb02c5548f6f781dd2e234a7ccffe0e13d9fb57f
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2023-04-27 02:40:07 -0400

    Time: 99 ms (80.00%), Space: 210.9 MB (80.00%) - LeetHub

---
+++ 1854-maximum-population-year/1854-maximum-population-year.rb
@@ -0,0 +1,14 @@
+# @param {Integer[][]} logs
+# @return {Integer}
+def maximum_population(logs)
+    hash = Hash.new(0)
+    logs.sort!
+    logs.each do |subarr|
+        (subarr[0]...subarr[1]).each do |ele|
+            hash[ele] += 1
+        end
+    end
+    p hash
+    ans = hash.max_by { |k, v| v }
+    ans[0]
+end
