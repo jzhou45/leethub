ab62d5c996735eed1367d1f39037ab92a41fc881
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2023-05-17 20:38:44 -0400

    Time: 72 ms (89.24%), Space: 16.4 MB (5.54%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.py
+++ 2652-sum-multiples/2652-sum-multiples.py
@@ -1,7 +1,11 @@
 class Solution:
     def sumOfMultiples(self, n: int) -> int:
-        res = 0
-        for i in range(1, n + 1):
-            if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
-                res += i
+        res = self.helper(n, 3).union(self.helper(n, 5).union(self.helper(n, 7)))
+        return sum(list(res))
+    
+    def helper(self, n, m):
+        if n < m: return set()
+        res = set()
+        for i in range(m, n + 1, m):
+            res.add(i)
         return res
