fde6ae0e193b44d4cf45074e0c55af42c24aed24
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2023-05-17 20:44:06 -0400

    Time: 190 ms (40.46%), Space: 16.3 MB (26.02%) - LeetHub

--- 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.py
+++ 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.py
@@ -1,8 +1,3 @@
 class Solution:
     def maximizeSum(self, nums: List[int], k: int) -> int:
-        res = 0
-        num = max(nums)
-        for _ in range(k):
-            res += num
-            num += 1
-        return res
+        return max(nums) * k + sum([i for i in range(1, k)])
