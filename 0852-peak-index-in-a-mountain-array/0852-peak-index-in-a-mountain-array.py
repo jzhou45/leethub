a1b02bca2fe4fd493476f4be3a3518e34134c4a0
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2023-07-25 09:52:18 -0400

    Time: 603 ms (41.44%), Space: 30.4 MB (10.03%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -1,10 +1,10 @@
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        if len(arr) == 1: return 0
         mid = len(arr) // 2
-        print(arr)
         
-        if len(arr) <= 3:
-            return arr.index(max(arr))
+        if len(arr) < 3:
+            return 0 if arr[0] > arr[1] else 1
         elif arr[mid - 1] > arr[mid]:
             return self.peakIndexInMountainArray(arr[:mid])
         else:
