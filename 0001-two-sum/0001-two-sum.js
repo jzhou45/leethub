e97bab5d860525bd22268ce9f2201c36533a1520
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2023-08-28 22:47:52 -0400

    Time: 63 ms (70.75%), Space: 42.5 MB (52.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,29 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    // for (const i in nums) {
-    //     const num = Number(i)
-    //     let matchingIndex = nums.slice(num + 1).indexOf(target - nums[num])
-    //     if (matchingIndex === -1) {
-    //         matchingIndex = null
-    //     } else {
-    //         matchingIndex += (num + 1)
-    //     }
-    //     console.log(matchingIndex)
-    //     if (matchingIndex) {
-    //         return [num, matchingIndex]
-    //     }
-    // }
     const map = {};
     
-    for (let i = 0; i < nums.length; i++) {
-        const num = nums[i];
-        const diff = target - num;
-        
-        if (diff in map){
-            return [map[diff], i];
+    for (let i = 0; i < nums.length; i++) {        
+        if (target - nums[i] in map){
+            return [map[target - nums[i]], i];
         } else {
-            map[num] = i;
+            map[nums[i]] = i;
         }
     }
 };
