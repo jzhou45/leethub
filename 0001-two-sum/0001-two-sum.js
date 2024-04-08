1e0ac5acfaaf4615c1fb608a113dee398db5cabb
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2024-04-08 00:42:33 -0400

    Time: 46 ms (97.97%), Space: 50.8 MB (47.24%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,14 +3,29 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    const map = {};
+
+/*
+loop through array
+find the difference between the target and my value
+save that into a hashmap, key = diff, value = index
+check if the value is in the hashmap, if true, breakout
+*/
+
+var twoSum = function(nums, target) { //[2,7,11,15], 9
+    const difference = {}; //7: 0
+    let res = []; //0, 1
     
-    for (let i = 0; i < nums.length; i++) {        
-        if (target - nums[i] in map){
-            return [map[target - nums[i]], i];
+    for (let i = 0; i < nums.length; i++) { // 1
+        const num = nums[i]; // 7
+        const diff = target - num; // 9 - 7 = 2
+        
+        if (num in difference) {
+            res = [difference[num], i];
+            break;
         } else {
-            map[nums[i]] = i;
+            difference[diff] = i;
         }
     }
+    
+    return res;
 };
