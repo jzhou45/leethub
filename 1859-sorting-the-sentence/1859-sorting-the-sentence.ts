39fbbca099dc96f0fbd6037fd298b97e5659077b
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2024-05-25 17:35:28 -0400

    Time: 50 ms (86.67%), Space: 50.1 MB (70.67%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.ts
@@ -0,0 +1,11 @@
+function sortSentence(s: string): string {
+    const arr = s.split(' ');
+    let res: string[] = Array(arr.length + 1).fill(' ');
+    
+    for (const word of arr) {
+        const lastChar = word[word.length - 1];
+        res[parseInt(lastChar)] = word.slice(0, word.length - 1);
+    }
+    
+    return res.slice(1).join(' ');
+};
