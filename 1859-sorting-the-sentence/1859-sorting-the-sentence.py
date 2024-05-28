62cd64775b2977ad661f32a7cb35f264732896c3
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2024-05-28 17:22:37 -0400

    Time: 28 ms (91.23%), Space: 16.5 MB (72.84%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        s = s.split(' ')
+        arr = [None] * (len(s) + 1)
+        
+        for word in s:
+            new_word, num = word[:-1], int(word[-1])
+            arr[num] = new_word
+        
+        return " ".join(arr[1:])
+            
