affdd3a2693bf37c24444ca140f7d1a6be3fe536
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2024-05-28 17:29:59 -0400

    Time: 47 ms (8.43%), Space: 16.4 MB (97.19%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,11 +1,21 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        s = s.split(' ')
-        arr = [None] * (len(s) + 1)
+        arr, temp = [], ""
         
-        for word in s:
-            new_word, num = word[:-1], int(word[-1])
-            arr[num] = new_word
+        def placeWord():
+            nonlocal arr, temp
+            num = int(temp[-1])
+            while len(arr) <= num:
+                arr.append(None)
+            arr[num] = temp[:-1]
+            return
         
+        for char in s:
+            if char == " ":
+                placeWord()
+                temp = ""
+            else:
+                temp += char
+        
+        placeWord()
         return " ".join(arr[1:])
-            
