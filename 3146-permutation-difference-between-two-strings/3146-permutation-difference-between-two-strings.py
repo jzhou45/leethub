ca3b780dde4e48cd1949ab6c0e5a1674c2d01514
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2024-06-20 21:02:12 -0400

    Time: 35 ms (78.11%), Space: 16.6 MB (40.43%) - LeetHub

--- 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.py
+++ 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.py
@@ -1,10 +1,9 @@
 class Solution:
     def findPermutationDifference(self, s: str, t: str) -> int:
-        s_dict = dict(zip(s, range(len(s))))
         t_dict = dict(zip(t, range(len(t))))
-        
+
         res = 0
-        for char in s:
-            res += abs(s_dict[char] - t_dict[char])
-        
+        for i, char in enumerate(s):
+            res += abs(i - t_dict[char])
+            
         return res
