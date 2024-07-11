6082a55b518ae4934e179133bf966ffc09432231
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2024-07-11 11:35:00 -0400

    Time: 79 ms (29.14%), Space: 52.8 MB (80.35%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.js
+++ 0733-flood-fill/0733-flood-fill.js
@@ -9,10 +9,14 @@ var floodFill = function(image, sr, sc, color) {
     const isInScope = (r, c) => r >= 0 && r < image.length && c >= 0 && c < image[0].length
     const originalColor = image[sr][sc]
     const isOriginalColor = (r, c) => originalColor === image?.[r]?.[c]
+    const seen = new Set()
     
     const search = (r, c) => {
+        const str = `${r}, ${c}`
+        if (seen.has(str)) return
         if (!(isInScope(r, c) && isOriginalColor(r, c))) return
         image[r][c] = color
+        seen.add(str)
         search(r - 1, c)
         search(r + 1, c)
         search(r, c - 1)
