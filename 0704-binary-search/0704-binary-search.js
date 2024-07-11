05d4fdc0b469746cef42e1849c21d85b308609c9
Jonathan Zhou <98574332+jzhou45@users.noreply.github.com>
2024-07-11 09:26:53 -0400

    Time: 60 ms (38.58%), Space: 52.1 MB (49.03%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,27 +4,15 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    if (nums.length === 1 && target !== nums[0]){
-        return -1;
-    }
-    let mid = Math.floor(nums.length / 2);
-    let left = nums.slice(0, mid);
-    let right = nums.slice(mid);
-
-    if (nums[mid] === target){
-        return mid;
-    } else if (target < nums[mid]){
-        let leftNum = search(left, target);
-        if (leftNum === -1){
-            return -1;
-        }
-        return leftNum;
+    if (nums.length <= 1 && nums?.[0] !== target) return -1
+    
+    const mid = Math.floor((nums.length - 1) / 2)
+    if (nums[mid] === target) return mid
+    
+    if (target < nums[mid]) {
+        return search(nums.slice(0, mid), target)
     } else {
-        search(right);
-        let rightNum = search(right, target);
-        if (rightNum === -1){
-            return -1;
-        }
-        return mid + rightNum;
+        const right = search(nums.slice(mid + 1), target)
+        return (right === -1) ? -1 : right + mid + 1
     }
 };
